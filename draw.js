d6b9032192b1f890b66c36de86a2a6eeb3814662
April-ying <april910909@gmail.com>
2024-01-21 18:21:01 +0800

    fix

--- draw.js
+++ draw.js
@@ -1,4 +1,5 @@
-//document.write("ok");
+
+
 // Canvas DOM 元素 
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
@@ -15,8 +16,8 @@ let y2= 0
 const hasTouchEvent = 'ontouchstart' in window ? true : false
 
 // 透過上方的 hasTouchEvent 來決定要監聽的是 mouse 還是 touch 的事件
-const downEvent = hasTouchEvent ? 'ontouchstart' : 'mousedown'
-const moveEvent = hasTouchEvent ? 'ontouchmove' : 'mousemove'
+const downEvent = hasTouchEvent ? 'touchstart' : 'mousedown'
+const moveEvent = hasTouchEvent ? 'touchmove' : 'mousemove'
 const upEvent = hasTouchEvent ? 'touchend' : 'mouseup'
 
 // 宣告 isMouseActive 為滑鼠點擊的狀態，因為我們需要滑鼠在 mousedown 的狀態時，才會監聽 mousemove 的狀態
@@ -35,7 +36,8 @@ canvas.addEventListener(downEvent, function(e){
   ctx.lineCap = 'round'
   ctx.lineJoin = 'round'
 })
-
+// 儲存線條軌跡的座標陣列
+var lineCoordinates = [];
 canvas.addEventListener(moveEvent, function(e){
       if(!isMouseActive){
         return
@@ -43,18 +45,29 @@ canvas.addEventListener(moveEvent, function(e){
       // 取得終點座標
       x2 = e.offsetX
       y2 = e.offsetY
-
+      
       // 開始繪圖
       ctx.beginPath()
       ctx.moveTo(x1, y1)
       ctx.lineTo(x2, y2)
       ctx.stroke()
 
+      // 將座標紀錄到軌跡陣列
+      lineCoordinates.push([x2,y2]);
+
       // 更新起始點座標
       x1 = x2
       y1 = y2
+      
 })
 
+//結束繪圖
 canvas.addEventListener(upEvent, function(e){
   isMouseActive = false
+  // 輸出軌跡座標
+  
+  //document.write(lineCoordinates);
+  // 清空軌跡座標陣列，以便下次繪製
+  lineCoordinates = [];
 })
+
