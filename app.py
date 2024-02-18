30ccf3a8009cc6482e014b2e49e3dc872660b5f7
April-ying <april910909@gmail.com>
2024-02-18 19:45:56 +0800

    成功了

--- app.py
+++ app.py
@@ -6,11 +6,21 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('index_socketio.html')
+    return render_template('index.html')
 
 @app.route('/confirm')
 def confirm():
     return render_template('confirm.html')
+
+@app.route('/images')
+def image():
+    return render_template('images2.html')
+
+@socketio.on('button_press')
+def handle_button_press(msg):
+    print('Button Pressed:', msg)
+    socketio.emit('button_press', msg)
+    
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- templates/c_button.js
+++ templates/c_button.js
@@ -1,3 +1,12 @@
 function confirm(){ 
+    const socket = io();
     
+
+        socket.on('button_press', (msg) => {
+            console.log('Button Pressed on the server:', msg);
+        });
+    
+        function sendButtonPress() {
+            socket.emit('button_press', 'Button Pressed!');
+        }
 }
